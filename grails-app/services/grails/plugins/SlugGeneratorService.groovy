b299e5908de0fd7931acf02d0601c5196a13877a
Andrey Antukh <niwi@niwi.be>
2013-03-28 21:44:59 +0100

    Add strict parameter to SlugGeneratorService.

--- grails-app/services/grails/plugins/SlugGeneratorService.groovy
+++ grails-app/services/grails/plugins/SlugGeneratorService.groovy
@@ -12,13 +12,17 @@ class SlugGeneratorService {
      * marks are removed from the value before generate the slug and then, if the slug exists in the database
      * a counter is added.
      *
-     * @param theClazz The domain class
-     * @param property The property of the domain class
-     * @param value The value from which generate the slug
-     * @return The unique slug generated or null if there is an error
+     * Note: this service uses SlugCodec for create a slug for a string and SlugCodec, by
+     * default is slighty permisive with unicode characters. If you really need a slug with only
+     * ascii characters, put a strict parameter to true.
+     *
+     * @param theClazz The domain class.
+     * @param property The property of the domain class.
+     * @param value The value from which generate the slug.
+     * @param strict Activates the strict mode (default false).
+     * @return The unique slug generated or null if there is an error.
      */
-    public String generateSlug(Class theClazz, String property, String value) {
-
+    public String generateSlug(Class theClazz, String property, String value, Boolean strict=false) {
         // Check if the class if a DomainClass
         if (!grailsApplication.isArtefactOfType("Domain", theClazz)) {
             return null
@@ -34,7 +38,7 @@ class SlugGeneratorService {
         }
 
         // Generate the initial slug and look for it
-        def initialSlug = SlugCodec.encode(value)
+        def initialSlug = SlugCodec.encode(value, strict)
         def instance = theClazz."findBy${property.capitalize()}"(initialSlug)
         if (!instance) {
             return initialSlug
@@ -42,9 +46,11 @@ class SlugGeneratorService {
 
         int c = 1
         def slug = ""
+
         while (true) {
             slug = "${initialSlug}-${c}"
             instance = theClazz."findBy${property.capitalize()}"(slug)
+
             if (!instance) {
                 return slug
             } else {
