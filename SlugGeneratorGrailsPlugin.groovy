963a4f7fe350780d09e2277bcb4711f38f3d1827
Iván López <lopez.ivan@gmail.com>
2014-06-22 20:16:35 +0200

    Update to version 0.4

--- SlugGeneratorGrailsPlugin.groovy
+++ SlugGeneratorGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class SlugGeneratorGrailsPlugin {
     // the plugin version
-    def version = "0.3.1"
+    def version = "0.4"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.0 > *"
     // the other plugins this plugin depends on
